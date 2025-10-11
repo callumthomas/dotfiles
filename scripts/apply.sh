43593dbc92ee0f9d83397a4146c5c16b11ae4f43
callum <callum>
2025-10-11 03:09:57 +0100

    nvim

---
+++ scripts/apply.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rm -rf ~/.config/hypr
+ln -s $(pwd)/hypr ~/.config/hypr
+
+rm -rf ~/.config/waybar
+ln -s $(pwd)/waybar ~/.config/waybar
+
+rm -rf ~/.config/nvim
+ln -s $(pwd)/waybar ~/.config/nvim
+
+rm ~/.zshrc
+ln -s $(pwd)/.zshrc ~/.zshrc

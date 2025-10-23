8ae9f76b1f4585f8d16ea4499b075aa9b8c60a44
callum <callum>
2025-10-23 10:42:56 +0100

    git configs

---
+++ scripts/git-config.sh
@@ -0,0 +1,6 @@
+git config --global user.name "callum"
+git config --global user.email "callum"
+git config --global core.editor "vim"
+git config --global pull.ff true
+git config --global pull.rebase false
+git config --global pull.autoSetupRemote true

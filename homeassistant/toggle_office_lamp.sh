65acca57f09c207b3777b8bb4c933b948cb44c10
callum <callum>
2025-11-16 13:50:36 +0000

    lamp

--- homeassistant/toggle_office_lamp.sh
+++ homeassistant/toggle_office_lamp.sh
@@ -4,7 +4,7 @@
 source "${BASH_SOURCE[0]%/*}/../.env"
 
 curl -X POST \
-  -H "Authorization: Bearer $HOMEASSISTANT_TOKEN" \
-  -H "Content-Type: application/json" \
-  -d "{\"entity_id\": \"light.cals_office_lamp\"}" \
-  http://192.168.1.3:8123/api/services/light/toggle
+	-H "Authorization: Bearer $HOMEASSISTANT_TOKEN" \
+	-H "Content-Type: application/json" \
+	-d "{\"entity_id\": \"light.cals_office_lamp\"}" \
+	http://$HOMEASSISTANT_SERVER:8123/api/services/light/toggle

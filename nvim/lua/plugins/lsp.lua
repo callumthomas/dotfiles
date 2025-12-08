d19cbbf387f8410cc36c8e64d7bdb5d177695f5a
callum <callum>
2025-12-08 10:07:07 +0000

    eslint

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -137,7 +137,17 @@ return {
 						-- Auto-fix on save
 						vim.api.nvim_create_autocmd("BufWritePre", {
 							buffer = bufnr,
-							command = "EslintFixAll",
+							callback = function()
+								client:request("workspace/executeCommand", {
+									command = "eslint.applyAllFixes",
+									arguments = {
+										{
+											uri = vim.uri_from_bufnr(bufnr),
+											version = vim.lsp.util.buf_versions[bufnr],
+										},
+									},
+								}, nil, bufnr)
+							end,
 						})
 					end,
 				})
